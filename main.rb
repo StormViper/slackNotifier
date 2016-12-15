75ed79081e5a8e7424b51e7a8821b4084480581b
Adam Parker <adam@Opss-Mac-mini-2.local>
2016-12-15 15:40:07 +0000

    First setup

---
+++ main.rb
@@ -0,0 +1,86 @@
+require 'slack-ruby-client'
+class AirDevBrake
+  def initialize(dev, air)
+    @@dev, @@air = dev, air
+
+    Slack.configure do |config|
+      config.token = "xoxb-116923201861-UZWuipiNsDKygGaiBzN9CCBR"
+    end
+    @@client = Slack::Web::Client.new
+    @@dev_user, @@air_user = @@dev.first, @@air.first
+    @@first_user = 1
+    get_user_on_channel(@@air_user, @@dev_user)
+end
+
+def get_user_on_channel(air, dev)
+  airbrake_user = air
+  devsupport_user = dev
+
+  call_api_slack(airbrake_user, devsupport_user)
+end
+
+def post_slack_tags(dev, air)
+  @@client.chat_postMessage(channel: '#airbrake', text: "#{air}, You're on Airbrake today.", as_user: true, parse: true, link_names: true)
+
+  @@client.chat_postMessage(channel: '#devsupport', text: "#{dev}, You're on Dev-support today.", as_user: true, parse: true, link_names: true)
+end
+
+def check_days
+  if Time.now.strftime("%A").downcase == "sunday" || Time.now.strftime("%A").downcase == "saturday"
+    return true
+  else
+    return false
+  end
+end
+
+def set_next_user
+  airbrake = @@air[@@first_user]
+  devsupport = @@dev[@@first_user]
+  @@first_user +=1
+  if @@first_user > 7
+    @@first_user = 0
+  end
+
+  return [airbrake, devsupport]
+end
+
+def call_api_slack(airbrake, devsupport)
+  airbrake, devsupport = airbrake, devsupport
+
+  if check_days
+    sleep(86400)
+    get_user_on_channel(airbrake, devsupport)
+  else
+    post_slack_tags(devsupport.capitalize, airbrake.capitalize)
+    next_user = set_next_user
+    sleep(86400)
+    get_user_on_channel(next_user[0], next_user[1])
+  end
+end
+
+end
+
+
+dev=[
+  '@bank.lee',
+  '@richardkettle ',
+  '@jacob',
+  '@francesca',
+  '@georgi',
+  '@richard',
+  '@nicola',
+  '@adam'
+]
+
+air=[
+  '@nicola',
+  '@adam',
+  '@georgi',
+  '@richard',
+  '@francesca',
+  '@jacob',
+  '@richardkettle',
+  '@bank.lee'
+]
+
+AirDevBrake.new(dev, air)
